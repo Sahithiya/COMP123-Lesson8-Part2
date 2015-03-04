c1ad9e7d2eb2f99590defadd4176fbdbebfa1b0e
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-03-04 08:57:24 -0500

    added WaitForKey method

--- COMP123-Lesson8-Part2/Program.cs
+++ COMP123-Lesson8-Part2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; //need this in order to perform File I/O
 
 namespace COMP123_Lesson8_Part2
 {
@@ -10,6 +11,21 @@ namespace COMP123_Lesson8_Part2
     {
         static void Main(string[] args)
         {
+
+            string prompt;
+
+
+        }
+
+        // UTILITY METHODS
+        private static void WaitForKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
